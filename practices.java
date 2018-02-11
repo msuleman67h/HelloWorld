0ff1a97d34e0720e4ae0db8c3f1af8a55b6bafab
Muhammad Suleman <36046698+msuleman67h@users.noreply.github.com>
2018-02-11 17:19:15 -0500

    Changes
    Acha ma na code ma ya changes kiae hain
    1. (Less Important change )Code indent kia ha (code ho horizontally buhat zaida lamba nhi hona chahie ka scroll na karna pare) Ya 
        actually book ma. Code ma jo cheez ha us ko wohi naam do enter ha kuch nhi pta chalta instead use dividend ya kuch aur (depends 
        on value) kyon ka code khud review karne ma asani hoti ha. (You probably know all of these stuff kyon ka code practice ka liae ha
        is liae rules follow nhi kiae) I used to do the same thing until I realized following rules makes debugging so much easy.   
        likha ho ha. and comment wahan karo jahan kam ho rha ha. 
    2. Ma na dividend and remainder ko combine kar dia ha kyon ka dekho
        User enters 8. 8 even ha. 8/2 = 4. Sahi ab jo 4 remainder ban gya ha jab loop dobara run hoga tu wo dividend ho ga
        4(dividend)/2(divisor) = 2(remainder) (line 27  dividend /= 2;	) . Ab jab loop dobara chale ga remainder phir dividend ban jae ga     
        2/2 = 1. 
        Tumhara dividend and remainder separate tha jab ka remainder har next iteration ma dividend ban jata ha. or for loop ma 
        choti si mistake thi ka agar remainder 1 ho tu loop na chale. our loop condition was agar remainder 1 ho tu loop chale mgr tum 
        na  remainder ki value 0 sa initialize ki hoi thi is liae loop kabhi chalta hi nhi tha.
        int  remainder = 0; and int  remainder; same hain. Java 2nd ma khud hi 0 value lga leta ha. Ya sab data types ka sath hota ha except 
        String. String ki koi default value nhi. (http://cs.fit.edu/~ryan/java/language/java-data.html) 
    3. Jab code likh tu eik paper pakar ka khud dimagh sa compile karo line by line and keep expected result and actual result in mind. 
        Agar kuch pochna ha whatsapp kar dena. :-)

--- practices.java
+++ practices.java
@@ -1,34 +1,42 @@
-package practices;
 
+/* Collatz Sequence Collatz Sequence
+ * In 1937, Lothar Collatz proposed that no matter what number you begin with, the sequence eventually reaches 1. 
+ * This is widely believed to be true, but has never been formally proved. Write a program CollatzSequence.c that 
+ * inputs a number from the user, and then displays the Collatz Sequence starting from that number. Stop when you 
+ * reach 1.
+ */
 import java.util.Scanner;
 
 public class practices {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		// Collatz Sequence Collatz Sequence
-//		Take any natural number n.
-//		 If n is even, divide it by 2 to get n / 2.
-//		 If n is odd, multiply it by 3 and add 1 to get 3n + 1.
-//		 Repeat the process indefinitely.
+		boolean loopRun = false;
+		
+		System.out.print("Enter a number: ");
+		
+		// Take any natural number n (dividend).
+		int dividend = input.nextInt();
+		
+		for (int z = 0; dividend != 1; z++) { // Repeat the process indefinitely until dividend becomes 1.
 
-//		In 1937, Lothar Collatz proposed that no matter what number you begin with, the sequence eventually reaches 1. This is widely believed to be true, but has never been formally proved.
-//		Write a program CollatzSequence.c that inputs a number from the user, and then displays the Collatz Sequence starting from that number. Stop when you reach 1.
-
-
-		int tem = 0;
-		System.out.println("Enter A number: ");
-		int enter = input.nextInt();
-
-		for (int z = 0;tem == 1; z++) {
-
-			if (enter % 2 == 0) {
-				tem = enter / 2;				//checking for even num
+			if (dividend % 2 == 0) { //checking for even number. If n is even, divide it by 2 to get n / 2.
+				/*
+				 * dividing dividend by 2 because in next iteration the value of remainder becomes dividend.
+				 */
+				dividend /= 2;
 			} else {
-				tem = (3 * enter) + 1;				// checking for odd num
+				dividend = (3 * dividend) + 1;  // If n is odd, multiply it by 3 and add 1 to get 3n + 1.
 			}
-			System.out.print(tem + "   ");
-
+			System.out.println("The answer is > "+dividend);
+			/*
+			 *  sets the values to true so that the program knows the value (dividend = 1) is from
+			 *  calculation not from user input.
+			 */
+			loopRun = true;
+		}
+		if(dividend == 1 && loopRun == false) {  // If the users enters 1 as dividend and loop didn't run
+			System.out.println("The answer is > "+dividend);
 		}
 	}
 
